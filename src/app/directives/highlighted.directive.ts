c84c87a5454276936da5df4ee9d2283751efda8f
Stefano Mazzolini <stefano.mazzolini@gmail.com>
2021-01-05 17:19:53 +0100

    dependency injection in depth

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,4 +1,5 @@
-import {Directive, EventEmitter, HostBinding, HostListener, Input, Output} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Directive, EventEmitter, Host, HostBinding, HostListener, Input, Output} from '@angular/core';
 
 @Directive({
     selector: '[highlighted]',
@@ -12,7 +13,13 @@ export class HighlightedDirective {
     @Output()
     toggleHighlight = new EventEmitter();
 
-    constructor() {
+    /* @Host() private service: AnyService */
+    /* 
+        @Host() decorator is used commonly when we want the Host component (e.g. CourseCardComponent) of the directive (e.g. [highlighted])
+        to access to the same instance of the service
+    */
+
+    constructor(/* @Host() private service: AnyService */) {
 
         console.log('Directive created..');
 
