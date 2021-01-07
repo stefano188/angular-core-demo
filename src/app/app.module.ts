00d78d045e4dd5daf4da2b19b96ff68ca32b4ed9
Stefano Mazzolini <stefano.mazzolini@gmail.com>
2021-01-07 17:23:24 +0100

    angular elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoursesModule } from './courses/courses.module';
+import { CourseTitleComponent } from './course-title/course-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CourseTitleComponent,
   ],
   imports: [
     BrowserModule, // contains inside CommonModule
